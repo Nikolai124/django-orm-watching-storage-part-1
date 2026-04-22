a9317b7d40a28f864f5b505dc58cf59106bb08ff
Nikolai124 <g4728281919@gmail.com>
2026-04-22 19:59:01 +0300

    скрыл секретные данные

--- settings.py
+++ settings.py
@@ -1,17 +1,21 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': os.geenv('DJANGO_HOST_DB'),
+        'PORT': os.getenv('DJANGO_PORT_DB'),
+        'NAME': os.getenv('DJANGO_NAME_DB'),
+        'USER': os.getenv('DJANGO_USER_DB'),
+        'PASSWORD': os.getenv('DJANGO_PASSWORD_DB'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 TIME_ZONE = 'Europe/Moscow'
 
